3262e5496f7acdd3776594b53d034676ac54eea4
Neel Patel <neelpatel.6531@gmail.com>
2019-12-15 11:54:12 +0530

    add new webpage

---
+++ precache-manifest.c0328768f3b00a78d5c19e22849c928b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc21ad8d043625e5d34796565874407",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48e1cfd2ffd31f6da6e0",
+    "url": "/static/css/2.c726b981.chunk.css"
+  },
+  {
+    "revision": "80ff0db26d6690eb0000",
+    "url": "/static/css/main.cc838053.chunk.css"
+  },
+  {
+    "revision": "48e1cfd2ffd31f6da6e0",
+    "url": "/static/js/2.c96bb646.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.c96bb646.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80ff0db26d6690eb0000",
+    "url": "/static/js/main.a682de03.chunk.js"
+  },
+  {
+    "revision": "776d3674cb8f74dafa6a",
+    "url": "/static/js/runtime-main.30a9b0eb.js"
+  },
+  {
+    "revision": "04558f3ab8b6ed682850a9aca2bcfa95",
+    "url": "/static/media/exactt.04558f3a.png"
+  }
+]);
